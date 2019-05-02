0ff6c63074704b4b4e83db26518beb54cefbcc89
sandrine <sandrinezocca@gmail.com>
2019-05-02 13:58:29 +0200

    add text field

--- src/components/EstimateForm.jsx
+++ src/components/EstimateForm.jsx
@@ -1,31 +1,39 @@
 import React, { Component } from 'react';
 
 class EstimateForm extends Component {
-    state = { 
-        estimateTitle: ''
-     };
+    state = {
+        id: "",
+        title: "",
+        customerFirstName: "",
+        customerLastName: "",
+        items: {}
+    };
 
     handleSubmit = evt => {
         evt.preventDefault();
         console.log('généré');
-        
+
     }
 
-    handleChange = evt => {
-        const title = evt.currentTarget.value;
-        console.log(title);
+    handleChange = (evt, name) => {
+        const value = evt.currentTarget.value;
+        console.log(JSON.stringify({name: value}));
+        console.log(JSON.stringify({[name]: value}));
         this.setState({
-            estimateTitle: title
+           [name]: value
         })
-        
+
     }
 
     render() {
         return (
             <>
                 <div>Nouveau devis</div>
-                <form onSubmit ={ this.handleSubmit }>
-                    <input type="text" name="estimate-title" id="estimate-title" placeholder="titre du devis" value={ this.state.estimateTitle } onChange={ evt => this.handleChange(evt) } />
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" name="id" id="id" placeholder="ID" value={this.state.id} onChange={evt => this.handleChange(evt, 'id')} /> <br/>
+                    <input type="text" name="title" id="title" placeholder="titre du devis" value={this.state.Title} onChange={evt => this.handleChange(evt,'title')} /> <br/>
+                    <input type="text" name="customerFirstName" id="customerFirstName" placeholder="prénom" value={this.state.customerFirstName} onChange={evt => this.handleChange(evt,'customerFirstName')} /> <br/>
+                    <input type="text" name="customerLastName" id="customerLastName" placeholder="nom" value={this.state.customerLastName} onChange={evt => this.handleChange(evt, 'customerLastName')} />
                     <button type="submit">générer le devis</button>
                 </form>
             </>
