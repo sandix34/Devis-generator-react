b9922ba0761a2fb838b274ed8ccd964cd9e517b9
sandrine <sandrinezocca@gmail.com>
2019-05-08 15:46:19 +0200

    pdf

--- src/components/item.jsx
+++ src/components/item.jsx
@@ -1,42 +1,64 @@
-import React from 'react';
+import React from "react";
 
-const Item = (props) => {
-    return (
-        <>
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <input
-                            type="text"
-                            name="description"
-                            value={props.item.description}
-                            onChange={evt => props.onItemChange(evt, props.item, "description")} />
-                    </div>
-                    <div className="col">
-                        <input
-                            type="number"
-                            name="quantité"
-                            value={props.item.quantity}
-                            onChange={evt => props.onItemChange(evt, props.item, "quantity")} />
-                    </div>
-                    <div className="col">
-                        <input
-                            type="text"
-                            name="taxe"
-                            value={props.item.taxe}
-                            onChange={evt => props.onItemChange(evt, props.item, "taxe")} />
-                    </div>
-                    <div className="col">
-                        <input
-                            type="text"
-                            name="amount"
-                            value={props.item.amount}
-                            onChange={evt => props.onItemChange(evt, props.item, "amount")} />
-                    </div>
-                </div>
-            </div>
-        </>
-    );
-}
+const Item = ({ item, onItemChange }) => {
+  const labels = {
+    one: {text: 'description', width: 110 },
+    two: {text: 'quantité', width: 50 },
+    three: {text: 'taxe', width: 50 },
+    four: {text: 'montant', width: 70 }
+  }
+  return (
+    <React.Fragment>
+      <div className="container" style={{marginLeft: 0}}>
+        <div className="row m-2">
+          <div className="col">
+            <label className="m-2" htmlFor={labels.one.text}>{labels.one.text}</label>
+            <input
+              type="text"
+              id={labels.one.text}
+              name="description"
+              value={item.text}
+              onChange={evt => onItemChange(evt, item, "description")}
+              style={{width: labels.one.width}}
+            />
+          </div>
+          <div className="col">
+            <label className="m-2" htmlFor={labels.two.text}>{labels.two.text}</label>
+            <input
+              type="number"
+              id={labels.two.text}
+              name="quantité"
+              value={item.quantity}
+              onChange={evt => onItemChange(evt, item, "quantity")}
+              style={{width: labels.two.width}}
+            />
+          </div>
+          <div className="col">
+            <label className="m-2" htmlFor={labels.three.text}>{labels.three.text}</label>
+            <input
+              type="text"
+              id={labels.three.text}
+              name="taxe"
+              value={item.taxe}
+              onChange={evt => onItemChange(evt, item, "taxe")}
+              style={{width: labels.three.width}}
+            />
+          </div>
+          <div className="col">
+            <label className="m-2" htmlFor={labels.four.text}>{labels.four.text}</label>
+            <input
+              type="text"
+              id={labels.four.text}
+              name="amount"
+              value={item.amount}
+              onChange={evt => onItemChange(evt, item, "amount")}
+              style={{width: labels.four.width}}
+            />
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default Item;
--- src/components/pdfMaker.js
+++ src/components/pdfMaker.js
@@ -1,58 +1,243 @@
-import React from 'react';
-import { Document, Page, Text, View, StyleSheet, PDFViewer } from '@react-pdf/renderer';
-import ReactDOM from 'react-dom';
+import React from "react";
+import {
+  Document,
+  Page,
+  Text,
+  View,
+  StyleSheet,
+  Font,
+  PDFViewer
+} from "@react-pdf/renderer";
+import ReactDOM from "react-dom";
 
 // Create styles
 const styles = StyleSheet.create({
   page: {
-    flexDirection: 'row',
-    backgroundColor: '#E4E4E4'
+    flexDirection: "column",
+    backgroundColor: "#E4E4E4"
+  },
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    marginLeft: "30"
   },
   section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1,
-    fontSize: 18,
-    width: 200,
-    '@media max-width: 400': {
-      width: 300,
-    },
-    '@media orientation: landscape': {
-      width: 400,
-    },
+    margin: 5,
+    padding: 5,
+    fontSize: 12,
+    flexDirection: 'row',
+    // border: '3px solid pink',
+  },
+  title: {
+    margin: 5,
+    fontSize: 15,
+    textAlign: "center",
+    backgroundColor: "#e4e4e4",
+    textTransform: "uppercase"
+  },
+  body: {
+    flexGrow: 1
+  },
+  row: {
+    flexDirection: "row",
+    width: "100%",
+    height: 130,
+    // border: "3px solid yellow"
+  },
+  rowSmallHeight: {
+    flexDirection: "row",
+    width: "100%",
+    height: 85,
+    // border: "3px solid yellow"
+  },
+  block: {
+    flexGrow: 1
+  },
+  text: {
+    width: "33.330%",
+    margin: 5,
+    fontFamily: "Oswald",
+    textAlign: "justify"
+  },
+  leftColumn: {
+    flexDirection: "column",
+    fontSize: 12,
+    marginLeft: 30,
+    marginRight: 15,
+    marginTop: 5,
+    height: 90,
+    // border: "3px solid red",
+  },
+  rightColumn: {
+    flexDirection: "column",
+    flexGrow: 10,
+    fontSize: 12,
+    marginLeft: 15,
+    marginRight: 30,
+    marginTop: 5,
+    height: 90,
+    // border: "3px solid blue",
+  },
+  line: {
+    fontFamily: "Oswald",
+    textAlign: "justify",
+    fontSize: 12,
+    margin: 5,
+    padding: 5,
+    width: 80,
+    flexDirection: "row",
+    // border: '3px solid orange'
   }
 });
 
-// Create Document Component
-const MyDocument = ({text}) => (
-  <Document>
-    <Page size="A4" style={styles.page}>
-        <View style={styles.section}>
-        <Text>Devis : { text.title }</Text>
-        <Text>N° : { text.id }</Text>
-        <Text>Client : { text.customerFirstName } { text.customerLastName }</Text>
-        <Text>Articles : </Text>
-        { Object.keys(text.items).map((key, index) => (
-          <Text key={key}>
-          {text.items[key].quantity} &nbsp;
-          {text.items[key].description} &nbsp;
-          {parseFloat(text.items[key].taxe) * 100.}% &nbsp;
-          {text.items[key].amount}€
-          </Text>
-        )) }
-      </View>
-    </Page>
-  </Document>
+Font.register(`../fonts/Roboto-Regular.ttf`, { family: "Roboto" });
+Font.register(
+  "https://fonts.gstatic.com/s/oswald/v13/Y_TKV6o8WovbUd3m_X9aAA.ttf",
+  { family: "Oswald" }
 );
 
-export const renderPDFInDOM = (text) => {
+// const Title = ({ children }) => <Text style={styles.title}>{children}</Text>;
+
+const generateCorrespondingSpace = sentence => ' '.repeat(sentence.length);
+
+
+const totals = {
+  totalWithoutTaxes: 0,
+  taxes: 0,
+  totalTaxeIncluded: 0
+}
+
+const totalWithoutTaxes = (items) => {
+  const resultWithoutTaxes = Object.keys(items)
+    .map(key => {
+      const amount = parseFloat(items[key].amount, 10);
+      const quantity = parseInt(items[key].quantity, 10);
+      return (amount * quantity);
+    })
+    .reduce((acc, curr) => {
+      return acc += curr;
+  }, 0);
+  totals.totalWithoutTaxes = parseFloat(resultWithoutTaxes.toFixed(2));
+  return totals.totalWithoutTaxes
+  
+};
+
+const totalTaxeIncluded = (items) => {
+  const resulTaxeIncluded = Object.keys(items)
+  .map(key => {
+    const amount = parseFloat(items[key].amount, 10)
+    const quantity = parseInt(items[key].quantity, 10);
+    const taxe = parseFloat(items[key].taxe, 10); 
+    totals.taxes += (amount * quantity * taxe);
+    const result = ((amount * quantity) + (amount * quantity * taxe));
+    console.log('result', result);
+    return result;
+  })
+  .reduce((acc, curr) => {
+    return acc += curr;
+  }, 0);
+  totals.totalTaxeIncluded = parseFloat(resulTaxeIncluded.toFixed(2));
+  return totals.totalTaxeIncluded;
+};
+
+// Create Document Component
+const MyDocument = ({ text }) => (
+<PDFViewer>
+  <Document>
+    <Page size="A4" style={styles.page}>
+      <View style={styles.row}>
+        <View style={styles.leftColumn}>
+          <Text>Code Concept</Text>
+          <Text>Cs 30 817</Text>
+          <Text>11 rue du clos courtel</Text>
+          <Text>35700 rennes</Text>
+        </View>
+        <View style={styles.rightColumn}>
+          <Text style={styles.title}>DEVIS</Text>
+        </View>
+      </View>
+
+      <View style={styles.row}>
+        <View style={styles.leftColumn} />
+        <View style={styles.rightColumn}>
+          <Text>Société et/ou nom du client</Text>
+          <Text>
+            Client : {text.customerFirstName} {text.customerLastName}
+          </Text>
+        </View>
+      </View>
+
+      <View style={styles.rowSmallHeight}>
+        <View style={styles.leftColumn}>
+          <Text>Devis : {text.title} </Text>
+          <Text>devis n°: {text.id} </Text>
+        </View>
+        <View style={styles.rightColumn} />
+      </View>
+
+      <View style={styles.rowSmallHeight}>
+        <View style={styles.leftColumn}>
+          <Text>Intitulé : {text.title} </Text>
+        </View>
+        <View style={styles.rightColumn} />
+      </View>
+
+      <View style={styles.container}>
+        <View style={styles.section}>
+          <Text style={styles.line}>Quantité</Text>
+          <Text style={styles.line}>Désignation</Text>
+          <Text style={styles.line}>Prix unitaire</Text>
+          <Text style={styles.line}>Prix total HT</Text>
+        </View>
+      </View>    
+      <View style={styles.container}>
+          {Object.keys(text.items).map((key, index) => (
+            <View style={styles.section} key={key}>
+              <Text style={styles.line}>{text.items[key].quantity}</Text>
+              <Text style={styles.line}>{text.items[key].description}</Text>
+              <Text style={styles.line}>{text.items[key].amount} €</Text>
+              <Text style={styles.line}>{parseInt(text.items[key].quantity, 10) *  text.items[key].amount} €</Text>
+              {/* <Text style={styles.line}>{parseFloat(text.items[key].taxe) * 100}%</Text> */}
+            </View>
+          ))}
+      </View>  
+      <View style={styles.container}>
+        <View style={styles.section}>
+          <Text style={styles.line}>{generateCorrespondingSpace('Quantité')}</Text>
+          <Text style={styles.line}>{generateCorrespondingSpace('Désignation')}</Text>
+          <Text style={styles.line}>Total HT</Text>
+          <Text style={styles.line}>{totalWithoutTaxes(text.items)} €</Text>
+        </View>
+      </View>  
+      <View style={styles.container}>
+        <View style={styles.section}>
+          <Text style={styles.line}>{generateCorrespondingSpace('Quantité')}</Text>
+          <Text style={styles.line}>{generateCorrespondingSpace('Désignation')}</Text>
+          <Text style={styles.line}>Total TTC</Text>
+          <Text style={styles.line}>{totalTaxeIncluded(text.items)} €</Text>
+        </View>
+      </View>  
+      <View style={styles.container}>
+        <View style={styles.section}>
+          <Text style={styles.line}>{generateCorrespondingSpace('Quantité')}</Text>
+          <Text style={styles.line}>{generateCorrespondingSpace('Désignation')}</Text>
+          <Text style={styles.line}>dont TVA</Text>
+          <Text style={styles.line}>{totals.taxes.toFixed(2)} €</Text>
+        </View>
+      </View>  
+    </Page>
+  </Document>
+  </PDFViewer>
+);
+
+export const renderPDFInDOM = estimateState => {
+  console.log(estimateState);
   const Wrapper = () => (
     <div>
-      < PDFViewer >
-        <MyDocument text={text}/>
-      </PDFViewer>
+      <MyDocument text={estimateState} />
     </div>
   );
-  
-  ReactDOM.render(<Wrapper />, document.getElementById('pdf'));
-}
+
+  ReactDOM.render(<Wrapper />, document.getElementById("pdf"));
+};
